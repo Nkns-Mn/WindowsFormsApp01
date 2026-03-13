2a53d84732f97405c6b43030053e61a55645bc2b
mana.tokorocchi8@gmail.com <mana.tokorocchi8@gmail.com>
2026-03-13 21:38:48 +0900

    課題【No.32-15】修正

--- WindowsFormsApp01/Form1.Designer.cs
+++ WindowsFormsApp01/Form1.Designer.cs
@@ -47,12 +47,13 @@
             // 
             this.button1.BackColor = System.Drawing.Color.Red;
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(76, 259);
+            this.button1.Location = new System.Drawing.Point(76, 258);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(98, 67);
             this.button1.TabIndex = 1;
             this.button1.Text = "Push!";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
